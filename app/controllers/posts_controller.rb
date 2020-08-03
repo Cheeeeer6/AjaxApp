7512b3c8c4665c019fdb155ac7f5ecd8b47a6635
Cheeeeer6 <remonchip45@gmail.com>
2020-08-03 14:47:09 +0900

    checkedアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,18 @@ class PostsController < ApplicationController
       Post.create(content: params[:content])
       redirect_to action: :index
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked then
+      # もともと既読の場合は未読にする
+      post.update(checked: false)
+    else
+      # もともと未読の場合は既読にする
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: {post: item}
+  end
 end
